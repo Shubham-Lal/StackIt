57ebb02dfc7163e4ce3cfc7f8060ae0a81ef8141
Jay9874 <jayprakashsharma225@gmail.com>
2025-07-12 14:52:07 +0530

    Removed old eslint

---
+++ client/src/components/Post.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Post () {
+  return (
+    <div>
+      {/* Left part of post */}
+      <div>
+        <p></p>
+      </div>
+    </div>
+  )
+}
