fbf0aee60d35b4710ff686435b4ec23e8bd74a6f
Shubham Lal <lal.shubham027@gmail.com>
2025-07-13 18:39:49 +0530

    wip: fix views counter bug

--- server/utils/cache.js
+++ server/utils/cache.js
@@ -1,4 +1,13 @@
 const NodeCache = require('node-cache');
-const viewCache = new NodeCache({ stdTTL: 86400 });
+
+const viewCache = new NodeCache({ stdTTL: 86400, checkperiod: 60 });
+
+viewCache.on('expired', (key, value) => {
+    console.log(`[Cache expired] Key: ${key}`);
+});
+
+viewCache.on('del', (key, value) => {
+    console.log(`[Cache deleted] Key: ${key}`);
+});
 
 module.exports = viewCache;
